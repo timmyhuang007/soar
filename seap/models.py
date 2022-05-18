701bf358b476707e7a7a5633994eb2a59c641198
timmyhuang007 <timmyhuang@wo.cn>
2022-05-18 10:56:39 +0800

    add models

--- seap/models.py
+++ seap/models.py
@@ -1,12 +1,34 @@
 from datetime import datetime, date
+from enum import IntEnum
 from ipaddress import ip_address
 from django.contrib.auth import User
 from django.db import models
 
 # Create your models here.
 class Event(models.Model):
+    class STATUS(IntEnum):
+        IGNORED = -1
+        TODO = 0
+        DOING = 1
+        CLOSED = 2
+
     ip = models.GenericIPAddressField(verbose_name='IP', protocol='both', unpack_ipv4=True, default=ip_address(0))
+    status = models.SmallIntegerField(verbose_name='事件状态', default=STATUS.TODO, choices=[
+        (STATUS.IGNORED, '已忽略'),
+        (STATUS.TODO, '待响应'),
+        (STATUS.DOING, '处理中'),
+        (STATUS.CLOSED, '已关闭'),
+    ])
+    report_user = models.ForeignKey(User, verbose_name='上报人', on_delete=models.SET_NULL, null=True)
     report_time = models.DateTimeField(verbose_name='上报时间', default=datetime.now)
-    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
     plan_date = models.DateField(verbose_name='计划日期', default=date.today)
-    
+    title = models.CharField(verbose_name='事件标题', max_length=64)
+    url = models.URLField(verbose_name='来源链接', max_length=256, blank=True)
+    screen_shot = models.ImageField(verbose_name='截屏', upload_to='screen_shot/%Y%m%d', max_length=256, null=True)
+    evidence = models.FileField(verbose_name='证据文件', upload_to='evidence/%Y%m%d', max_length=256, null=True)
+    extension = models.JSONField(verbose_name='拓展信息', null=True)
+
+class Comment(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(verbose_name='评论')
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
--- seap/serializers.py
+++ seap/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework.serializers import ModelSerializer
-from seap.models import Event
+from seap.models import Event, Comment
 
 class EventSerializer(ModelSerializer):
     class Meta:
         model = Event
+
+class CommentSerizlizer(ModelSerializer):
+    class Meta:
+        model = Comment
