56fd67af02e60d0bd54e3ce1ea776c350d88031f
timmyhuang007 <timmyhuang@wo.cn>
2022-05-18 14:21:57 +0800

    soar serializers ok

--- soar/serializers.py
+++ soar/serializers.py
@@ -1,16 +1,19 @@
 from django.contrib.auth.models import User, Group
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import ModelSerializer
 from soar.models import Environment
 
 # Serializers define the API representation.
-class UserSerializer(HyperlinkedModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
+        fields = ['id','is_superuser','username','first_name','last_name','email','is_staff','is_active']
         
-class GroupSerializer(HyperlinkedModelSerializer):
+class GroupSerializer(ModelSerializer):
     class Meta:
         model = Group
+        fields = ['id','name']
 
-class EnvironmentSerializer(HyperlinkedModelSerializer):
-    class Meata:
+class EnvironmentSerializer(ModelSerializer):
+    class Meta:
         model = Environment
+        fields = '__all__'
--- soar/urls.py
+++ soar/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from soar.views import UserViewSet, GroupViewSet, EnvironmentViewSet
+from soar.views import EnvironmentViewSet, GroupViewSet, UserViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = DefaultRouter()
