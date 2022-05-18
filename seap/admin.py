4a6b3cc943d8db32040c4bc1dc2e3f38f1fe2bb3
timmyhuang007 <timmyhuang@wo.cn>
2022-05-18 11:44:13 +0800

    add admin page

--- seap/admin.py
+++ seap/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from seap.models import Event, Comment
 
 # Register your models here.
+admin.site.register([Event, Comment])
---
+++ soar/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from soar.models import Environment
+
+# Register your models here.
+admin.site.register([Environment,])
