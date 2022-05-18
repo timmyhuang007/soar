f7ee9fda0e4a0f4d85e7e4782e412e2b6aad466f
timmyhuang007 <timmyhuang@wo.cn>
2022-05-18 10:21:15 +0800

    add media root

--- seap/models.py
+++ seap/models.py
@@ -1,7 +1,12 @@
+from datetime import datetime, date
+from ipaddress import ip_address
 from django.contrib.auth import User
 from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    ip = models.IPAddressField()
-
+    ip = models.GenericIPAddressField(verbose_name='IP', protocol='both', unpack_ipv4=True, default=ip_address(0))
+    report_time = models.DateTimeField(verbose_name='上报时间', default=datetime.now)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+    plan_date = models.DateField(verbose_name='计划日期', default=date.today)
+    
