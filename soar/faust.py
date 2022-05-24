2a046dd4935f8812658919000576378e6e60cfc3
timmyhuang007 <timmyhuang@wo.cn>
2022-05-24 09:47:19 +0800

    faust ok

--- soar/faust.py
+++ soar/faust.py
@@ -2,5 +2,6 @@ import faust
 from attrdict import AttrDict
 from .settings import PROJECT_NAME, MY_FAUST, MY_APPS
 
-app = faust.App(PROJECT_NAME, autodiscover=MY_APPS)
+app = faust.App(PROJECT_NAME)
 app.config_from_object(AttrDict(MY_FAUST))
+app.discover(*MY_APPS, ignore=['rest_framework'])
--- soar/models.py
+++ soar/models.py
@@ -1,11 +1,11 @@
-from django.db import models
+from django.db import models as M
 from django_cryptography.fields import encrypt
 from versionfield import VersionField
 
 # Create your models here.
-class Environment(models.Model):
-    name = models.CharField(verbose_name='环境变量名', max_length=32)
-    value = encrypt(models.TextField(verbose_name='环境变量值', max_length=1024))
+class Environment(M.Model):
+    name = M.CharField(verbose_name='环境变量名', max_length=32)
+    value = encrypt(M.TextField(verbose_name='环境变量值', max_length=1024))
     version = VersionField(verbose_name='版本', default='0.0', blank=True)
 
     def __str__(self):
