e68f735d88f9ebeb5bf96241ab55f421cb3e0fce
timmyhuang007 <timmyhuang@wo.cn>
2022-05-31 10:14:01 +0800

    use is_valid() before save()

--- seap/agents.py
+++ seap/agents.py
@@ -1,4 +1,3 @@
-import json
 import logging
 from traceback import format_exc
 from typing import AsyncIterable, Optional
@@ -7,11 +6,13 @@ from seap.models import Event
 from seap.serializers import EventSerializer
 
 logger = logging.getLogger(f'faulst.{__name__}')
+
 @app.agent(app.topic('soar_events'), value_serializer='raw', concurrency=1)
 async def event_serialize(stream:AsyncIterable) -> Optional[Event]:
     async for value in stream:
         try:
             event = EventSerializer(data=value)
+            event.is_valid(raise_exception=True)
             event.save()
         except:
             logger.warning(format_exc())
