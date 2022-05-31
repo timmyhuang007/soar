54c24f8586eeb9f0c719a5baf11a2861adf42343
timmyhuang007 <timmyhuang@wo.cn>
2022-05-31 10:03:28 +0800

    use logger

--- seap/agents.py
+++ seap/agents.py
@@ -1,12 +1,19 @@
 import json
-from typing import AsyncIterable
+import logging
+from traceback import format_exc
+from typing import AsyncIterable, Optional
 from soar import faust_app as app
 from seap.models import Event
 from seap.serializers import EventSerializer
 
+logger = logging.getLogger(f'faulst.{__name__}')
 @app.agent(app.topic('soar_events'), value_serializer='raw', concurrency=1)
-async def event_serialize(stream:AsyncIterable) -> Event:
+async def event_serialize(stream:AsyncIterable) -> Optional[Event]:
     async for value in stream:
-        event = EventSerializer(data=json.loads(value))
-        event.save()
+        try:
+            event = EventSerializer(data=value)
+            event.save()
+        except:
+            logger.warning(format_exc())
+            event = None
         yield event
