32cc7c32e2c48f2b6d4bba6540aae8d455204af1
timmyhuang007 <timmyhuang@wo.cn>
2022-06-19 10:02:31 +0800

    move agents definition to __main__

--- seap/agents.py
+++ seap/agents.py
@@ -1,21 +1,60 @@
+import json
 import logging
+from datetime import datetime
 from traceback import format_exc
-from typing import AsyncIterable, Optional
+from typing import AsyncIterable, Tuple
+
 from asgiref.sync import sync_to_async
-from soar import faust_app as app
-from seap.models import Event
+from benedict import benedict
+from elasticsearch import AsyncElasticsearch
+from elasticsearch.helpers import async_bulk
+from faust import Stream
+
 from seap.serializers import EventSerializer
+from soar import faust_app as app
+from soar.settings import MY_ELASTICSEARCH
 
 logger = logging.getLogger(f'faulst.{__name__}')
 
-@app.agent(app.topic('soar_events'), value_serializer='raw', concurrency=1)
-async def event_serialize(stream:AsyncIterable) -> Optional[Event]:
+class ElasticsearchSink:
+    def __init__(self, topic:str):
+        self.topic = topic
+        self.es = AsyncElasticsearch(
+            hosts = MY_ELASTICSEARCH['hosts'],
+            verify_certs = False,
+            ssl_show_warn = False,
+            http_auth = (MY_ELASTICSEARCH['username'], MY_ELASTICSEARCH['password'])
+        )
+    
+    def expand(self, record:dict) -> Tuple[str,str]:
+        record['@timestamp'] = datetime.now().astimezone()
+        index = json.dumps({
+            'index': {
+                '_index': f"{self._topic}-{record['@timestamp'].strftime('%Y.%m.%d')}"
+            }
+        })
+        data = benedict(record).to_json()
+        return index, data
+
+    async def bulk(self, stream:Stream):
+        async for records in stream.take(100, within=10):
+            yield await async_bulk(
+                client = self.es,
+                actions = records,
+                expand_action_callback = self.expand,
+                stats_only = True,
+                max_retries = 3
+            )
+
+async def event_serialize(stream:AsyncIterable) -> dict:
     async for value in stream:
         try:
-            event = EventSerializer(data=value)
-            await sync_to_async(event.is_valid)(raise_exception=True)
-            await sync_to_async(event.save)()
+            serializer = EventSerializer(data=value)
+            await sync_to_async(serializer.is_valid)(raise_exception=True)
+            event = (await sync_to_async(serializer.save)()).__dict__
+            event.pop('_state')
         except:
-            logger.warning(format_exc())
-            event = None
+            msg = format_exc()
+            logger.warning(msg)
+            event = {'message': msg}
         yield event
--- soar/__main__.py
+++ soar/__main__.py
@@ -1,6 +1,13 @@
+from seap.agents import ElasticsearchSink, event_serialize
+
 from .faust import app
 from .settings import MY_APPS
 
 if __name__ == '__main__':
+    topic = 'soar_events'
+    es_sink = app.agent(app.channel())(ElasticsearchSink(topic).bulk)
+    event_agent = app.agent(
+        app.topic(topic), value_serializer='raw', concurrency=1, sink=[es_sink]
+    )(event_serialize)
     app.discover(*MY_APPS, ignore=['rest_framework'])
     app.main()
